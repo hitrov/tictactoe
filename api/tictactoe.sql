5769d3da0c4bf1f436a260841bfe55415defbb5b
Alexander Hitrov <aleksander.hitrov@betgames.tv>
2018-01-21 17:53:01 +0200

    SQL dump updated.

--- api/tictactoe.sql
+++ api/tictactoe.sql
@@ -30,6 +30,15 @@ CREATE TABLE `player` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `player`
+--
+
+LOCK TABLES `player` WRITE;
+/*!40000 ALTER TABLE `player` DISABLE KEYS */;
+/*!40000 ALTER TABLE `player` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `game`
 --
 
@@ -53,6 +62,15 @@ CREATE TABLE `game` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `game`
+--
+
+LOCK TABLES `game` WRITE;
+/*!40000 ALTER TABLE `game` DISABLE KEYS */;
+/*!40000 ALTER TABLE `game` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `move`
 --
 
@@ -73,6 +91,49 @@ CREATE TABLE `move` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `move`
+--
+
+LOCK TABLES `move` WRITE;
+/*!40000 ALTER TABLE `move` DISABLE KEYS */;
+/*!40000 ALTER TABLE `move` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `telegram`
+--
+
+DROP TABLE IF EXISTS `telegram`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `telegram` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `telegram_id` int(11) unsigned NOT NULL,
+  `player_id` int(11) unsigned DEFAULT NULL,
+  `game_id` int(10) unsigned DEFAULT NULL,
+  `username` varchar(128) DEFAULT NULL,
+  `first_name` varchar(128) DEFAULT NULL,
+  `waiting_for_action` enum('name','new_game','move','') DEFAULT NULL,
+  `bearer_token` varchar(1024) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `player_id` (`player_id`),
+  KEY `game_id` (`game_id`),
+  CONSTRAINT `telegram_ibfk_1` FOREIGN KEY (`player_id`) REFERENCES `player` (`id`),
+  CONSTRAINT `telegram_ibfk_2` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `telegram`
+--
+
+LOCK TABLES `telegram` WRITE;
+/*!40000 ALTER TABLE `telegram` DISABLE KEYS */;
+/*!40000 ALTER TABLE `telegram` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
@@ -81,4 +142,4 @@ CREATE TABLE `move` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-01-20 20:01:22
+-- Dump completed on 2018-01-21 17:51:30
